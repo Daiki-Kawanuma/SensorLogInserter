5edd32dd83183ba96724ea5fe33b3f87e4fbc055
Toshiaki Uemura <uemura-toshiaki-xh@ynu.jp>
2016-12-19 17:13:32 +0900

    modify Hagimotocalculator

--- SensorLogInserterRe/Inserters/Components/HagimotoEcologCalculator.cs
+++ SensorLogInserterRe/Inserters/Components/HagimotoEcologCalculator.cs
@@ -17,15 +17,15 @@ namespace SensorLogInserterRe.Inserters.Components
         private static readonly double Rho = 1.22;
         private static readonly double Myu = 0.015;
 
-        public static DataTable CalcEcolog(DataRow tripRow, InsertDatum datum, InsertConfig config)
+        public static DataTable CalcEcolog(DataRow tripRow, InsertDatum datum, InsertConfig config, bool mapmatching)
         {
             var correctedGpsTable = new DataTable();
-            if (config.Correction == InsertConfig.GpsCorrection.SpeedLPFMapMatching) //補正GPS取得元変更
+            if (config.Correction == InsertConfig.GpsCorrection.SpeedLPFMapMatching && !mapmatching) //補正GPS取得元変更
             {
                 correctedGpsTable = CorrectedGpsSpeedLPF005MMDao.GetNormalized(tripRow.Field<DateTime>(TripsDao.ColumnStartTime),
                 tripRow.Field<DateTime>(TripsDao.ColumnEndTime), datum);
             }
-            else if(config.Correction == InsertConfig.GpsCorrection.MapMatching)
+            else if(mapmatching)
             {
                 correctedGpsTable = CorrectedGpsMMDao.GetNormalized(tripRow.Field<DateTime>(TripsDao.ColumnStartTime),
                 tripRow.Field<DateTime>(TripsDao.ColumnEndTime), datum);
